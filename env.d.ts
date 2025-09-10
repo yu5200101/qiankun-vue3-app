7da5b093a17a815d279c94b3786a0e5b074e0edb
yuruyuan <1377151258@qq.com>
2025-09-10 18:45:11 +0800

    first commit

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  VITE_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import qiankun from 'vite-plugin-qiankun';
 import path from 'path'
 
 const useDevMode = true; // 开启时禁用 HMR
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    qiankun('vue-app', { useDevMode })
-  ],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
-  },
-  server: {
-    port: 3002,
-    cors: true, // 允许跨域
-    headers: { 'Access-Control-Allow-Origin': '*' }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') // 加载所有变量（包括非 VITE_ 前缀）
+  return {
+    plugins: [
+      vue(),
+      qiankun('vue-app', { useDevMode })
+    ],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, './src') }
+    },
+    server: {
+      port: 3002,
+      cors: true, // 允许跨域
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    },
+    base: process.env.NODE_ENV === 'production' ? env.VITE_BACKEND_URL : '/' // 使用 loadEnv 加载的变量
   }
 })
